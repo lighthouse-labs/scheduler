dc609739878d83a45f96b89598d25479c6043d80
Karl Jensen <karljensen@gmail.com>
2019-07-31 00:19:44 -0700

    Updating jest-dom to @testing-library/jest-dom

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, fireEvent } from "react-testing-library";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 import Button from "components/Button";
 
