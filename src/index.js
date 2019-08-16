3fe1dc098550a0a62ac91cec614339b88ac49c88
Simon Garber <Simon.garber@gmail.com>
2019-08-15 22:30:51 -0400

    preparing the app for netlify deployment

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'index.scss';
 import Application from 'components/Application';
+import axios from 'axios';
 
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 ReactDOM.render(<Application />, document.getElementById('root'));
