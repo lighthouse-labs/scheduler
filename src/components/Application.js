17a1fbf4f1731e79e6da6dfc5e900306e25f5ea0
Khurram Virani <kvirani@gmail.com>
2019-09-06 13:17:02 -0700

    activity rename affects comment

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,7 +6,7 @@ export default function Application(props) {
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Environment Setup" activity. */}
+        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
