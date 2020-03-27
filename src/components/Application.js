20c0c648154ed7f6c7116db1e4268f995ba619d3
nicholasyyu <nicholas.yu24@gmail.com>
2020-03-26 23:35:52 -0700

    changes

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
+import DayList from "components/DayList";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        <nav>
+          <img
+            className="sidebar sidebar--centered"
+            src="images/logo.png"
+            alt="Interview Scheduler"
+          />
+          <DayList
+            days={days}
+            day={day}
+            setDay={setDay}
+          />
+        </nav>
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,7 +5,8 @@ import DayListItem from "components/DayListItem";
 export default function DayList(props) {
   const mappedDays = props.days.map(day => {
     return (
-      <DayListItem 
+      <DayListItem
+        key={day.id}
         name={day.name} 
         spots={day.spots} 
         selected={day.name === props.day}
